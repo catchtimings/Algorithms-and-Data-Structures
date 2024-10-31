065e2a1c5b027b8ef15a2917e5b693de41c96e24
catchtimings <chindina.63@gmail.com>
2024-10-31 16:44:14 +0400

    add try-catch into main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,12 +3,32 @@
 #include "function.h"
 
 int main() {
-	int array[] = { 1,2,3,4 };
+	try 
+	{
+		int array[] = { 1, 2, 3, 4, 5 };
+		double array_d[] = { 2.45, 6.46, 3.29 };
 
-	Polynom<int> new_polynom(array, 3);
+		Polynom<int> p1(array, 4);
+		Polynom<double> p2(array_d, 2);
 
-	std::cout << new_polynom;
-	std::cout << "Roots: " << *find_real_roots(new_polynom);
 
-	return 0;
+		std::cout << p1;
+		std::cout << p2;
+
+		double* result = find_real_roots(p2);
+		std::cout << result;
+	}
+	catch (const std::invalid_argument& ia) {
+		std::cerr << ia.what() << std::endl;
+	}
+	catch (const std::out_of_range& ooa) {
+		std::cerr << ooa.what() << std::endl;
+	}
+	catch (const std::logic_error& le) {
+		std::cerr << le.what() << std::endl;
+	}
+	catch (...) {
+		std::cerr << "Something went wrong..." << std::endl;
+	}
+
 }
