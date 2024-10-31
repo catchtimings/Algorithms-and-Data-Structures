f5f4430c4abc335569cf6f08012b27be578296e3
catchtimings <chindina.63@gmail.com>
2024-10-31 18:58:15 +0400

    added demonstration of all operators

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -5,20 +5,23 @@
 int main() {
 	try 
 	{
-		int array[] = { 1, 2, 3, 4, 5 };
-		double array_d[] = { 2.45, 6.46, 3.29 };
+		int array1[] = { 1, 2, 3, 4, 5 };
+		int array2[] = { 10,12,13,14 };
+		int num = 10;
 
-		Polynom<int> p1(array, 4);
-		Polynom<int> p_1(array, 4);
-		Polynom<double> p2(array_d, 2);
+		Polynom<int> p1(array1, 4);
+		Polynom<int> p2(array2, 3);
+
+		std::cout << "Polynom 1: " << p1 << std::endl;
+		std::cout << "Polynom 2: " << p2 << std::endl;
+		std::cout << "Polynom 1 + Polynom 2: " << p1 + p2 << std::endl;
+		std::cout << "Polynom 1 - Polynom 2: " << p1 - p2 << std::endl;
+		std::cout << "Polynom 1 * num: " << p1 * num << std::endl;
+		std::cout << "num * Polynom 1: " << num * p1 << std::endl;
 
 
-		std::cout << p1 << std::endl;
-		std::cout << p2 << std::endl;
-		std::cout << p1 + p_1 << std::endl;
-
-		double* result = find_real_roots(p2);
-		std::cout << result;
+		int* roots = find_real_roots(p2);
+		std::cout << "Roots: " << *roots << std::endl;
 	}
 	catch (const std::invalid_argument& ia) {
 		std::cerr << ia.what() << std::endl;
