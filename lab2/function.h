f038f9e497b221b90ecf3dfa67521f3a2b997f34
catchtimings <chindina.63@gmail.com>
2024-11-16 19:08:43 +0400

    add function

--- lab2/function.h
+++ lab2/function.h
@@ -1,2 +1,34 @@
-//#pragma once
-//#include "linkedlist.h"
+#pragma once
+#include <string>
+#include <ctime>
+#include "linkedlist.h"
+
+
+std::string choose_names() {
+	std::string names[] = { "Alexander", "Dmitriy", "Elizaveta", "Marina", "Semen", "Vadim", "Yulia" };
+	size_t size = sizeof(names) / sizeof(names[0]);
+	return names[rand() % size];
+}
+
+void generate_book(LinkedList<std::string>* book,
+					const size_t& apps_count,
+					const size_t& max_residents_count) {
+
+	if (book->get_head() != nullptr)
+		throw std::logic_error("Book must be empty");
+
+	for (size_t i = 0; i < apps_count; ++i) {
+		size_t residents_count = rand() % (max_residents_count);
+		for (size_t j = 0; j < residents_count; ++j) {
+			book[i].push_tail(choose_names());
+		}
+	}
+}
+
+void print_book(const LinkedList<std::string>* book) {
+	size_t count = 1;
+	for (Node<std::string>* ptr = book->get_head(); ptr != nullptr; ptr = ptr->next) {
+		std::cout << "Appartment: " << count << "\tResident names: " << book[count] << std::endl;
+		count++;
+	}
+}
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include "linkedlist.h"
+#include "function.h"
+
+
 
 int main() {
 	try {
-		LinkedList<double> lst;
+		/*LinkedList<double> lst;
 		lst.push_tail(1);
 		lst.push_tail(6);
 		lst.push_tail(90);
 		lst.push_tail(19);
 		
 		LinkedList<double> lst2 = lst;
-		std::cout << lst2;
+		std::cout << lst2;*/
+
+		srand(static_cast<size_t>(time(0)));
+		const size_t MAX_APPS = 100;
+		const size_t MAX_RESIDENTS = 10;
+
+		LinkedList<std::string> book[MAX_APPS];
+		generate_book(book, MAX_APPS, MAX_RESIDENTS);
+
+		print_book(book);
 
 	}
 	catch (const std::exception& e) {
