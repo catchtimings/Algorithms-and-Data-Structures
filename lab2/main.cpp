d5e0094d8d3a60a0fe38c9e721901b2549ce496a
catchtimings <chindina.63@gmail.com>
2024-11-24 14:51:46 +0400

    add checks to all methods and check function

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include "linkedlist.h"
+#include "function.h"
 
 int main() {
 	try {
-		LinkedList<double> lst;
-		lst.push_tail(1);
-		lst.push_tail(6);
-		lst.push_tail(90);
-		lst.push_tail(19);
+		{
+			LinkedList<double> lst;
+			lst.push_tail(1);
+			lst.push_tail(2);
+			lst.push_tail(3);
+			lst.push_tail(4);
+			std::cout << "List: " << lst << std::endl;
 
-		std::cout << lst[1]->data;
+			lst.push_head(52);
+			std::cout << "List: " << lst << std::endl;
+
+			lst.pop_tail();
+			std::cout << "List: " << lst << std::endl;
+			
+			lst.delete_node(2);
+			std::cout << "List: " << lst << std::endl;
+		}
+
+		{
+			srand(static_cast<size_t>(time(0)));
+			const size_t MAX_APPS = 100;
+			const size_t MAX_RESIDENTS = 7;
+
+			LinkedList<std::string>* book = generate_book(MAX_APPS, MAX_RESIDENTS);
+
+			print_book(book, MAX_APPS);
+
+			delete[] book;
+		}
 	}
 	catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
