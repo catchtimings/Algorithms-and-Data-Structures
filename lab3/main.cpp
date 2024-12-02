b7d5eed8f84d77c48667e088eadf10ebdd6bfdee
catchtimings <chindina.63@gmail.com>
2024-12-02 19:31:34 +0400

    add stats

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -5,8 +5,10 @@
 int main() {
 	try {
 		srand(static_cast<int>(time(0)));
-		//std::vector<int> vect = { 6, 59, 236, 0 }; //общий случай для проверок
+		std::vector<int> vect = { 6, 59, 236, 0 }; //общий случай для проверок
 		
+
+		//Сортировка вставками
 		{
 			std::vector <int> v(5);
 			generate_vector(v);
@@ -14,11 +16,14 @@ int main() {
 			std::cout << "Before insert sort: ";
 			print_vector(v);
 
-			insert_sort(v);
+			stats s = insert_sort(v);
 			std::cout << "After insert sort: ";
 			print_vector(v);
+
+			std::cout << s;
 		}
 		std::cout << '\n';
+		//Сортировка расчёской
 		{
 			std::vector <int> v(10);
 			generate_vector(v);
@@ -26,20 +31,24 @@ int main() {
 			std::cout << "Before comb sort: ";
 			print_vector(v);
 
-			insert_sort(v);
+			stats s = comb_sort(v);
 			std::cout << "After comb sort: ";
 			print_vector(v);
+			std::cout << s;
 		}
 		std::cout << '\n';
+		//Сортировка слиянием
 		{
 			std::vector <int> v(10);
 			generate_vector(v);
+
 			std::cout << "Before merge sort: ";
 			print_vector(v);
 
-			merge_sort(v);
+			stats s = merge_sort(v);
 			std::cout << "After merge sort: ";
 			print_vector(v);
+			std::cout << s;
 		}
 	}
 	catch (std::exception& e) {
--- lab3/task1.h
+++ lab3/task1.h
@@ -3,6 +3,16 @@
 #include <vector>
 #include <stdexcept>
 
+struct stats {
+	size_t comparison_count = 0;
+	size_t copy_count = 0;
+
+	friend std::ostream& operator<<(std::ostream& os, const stats& s) {
+		os << "Comparison count: " << s.comparison_count << "\tCopy count: " << s.copy_count << '\n';
+		return os;
+	}
+};
+
 void generate_vector(std::vector<int>& v) {
 	for (size_t i = 0; i < v.size(); ++i) {
 		v[i] = rand() % 100 + 1;
@@ -10,71 +20,91 @@ void generate_vector(std::vector<int>& v) {
 }
 
 void print_vector(const std::vector<int>& v) {
-	for (size_t i = 0; i < v.size(); ++i) {
-		std::cout << v[i] << " ";
+	for (int num : v) {
+		std::cout << num << " ";
 	}
 	std::cout << '\n';
 }
 
-void insert_sort(std::vector<int>& v) {
-	if (v.size() == 0) return;
+stats insert_sort(std::vector<int>& v) {
+	stats s;
+	if (v.size() == 0) return s;
 
+	
 	for (size_t i = 1; i < v.size(); ++i) {
 		int key = v[i];
 		size_t j = i;
 		while (j > 0 && v[j-1] > key) {
 			std::swap(v[j], v[j - 1]);
 			--j;
+			s.comparison_count++;
+			s.copy_count += 3;
 		}
 		v[j] = key; 
 	}
+
+	return s;
 }
 
-void comb_sort(std::vector<int>& v) {
-	if (v.size() == 0) return;
+stats comb_sort(std::vector<int>& v) {
+	stats s;
+	if (v.size() == 0) return s;
 
 	//const double factor = 1.247;
-	double step = v.size() - 1;
+	size_t step = v.size() - 1;
 
 	while (step >= 1) {
 		for (size_t i = 0; i + step < v.size(); ++i) {
 			if (v[i] > v[i + step]) {
 				std::swap(v[i], v[i + step]);
+				s.comparison_count++;
+				s.copy_count += 3;
 			}
 		}
 		--step;
 	}
+	return s;
 }
 
-void merge(const std::vector<int>& v1, const std::vector<int>& v2, std::vector<int>& v) {
+void merge(const std::vector<int>& v1, const std::vector<int>& v2, std::vector<int>& v, stats& s) {
 	size_t i = 0, j = 0, k = 0;
 
 	while (i < v1.size() && j < v2.size()) {
+		s.comparison_count++;
 		if (v1[i] <= v2[j]) {
 			v[k++] = v1[i++];
 		}
 		else {
 			v[k++] = v2[j++];
 		}
+		s.copy_count++;
 	}
 
 	while (i < v1.size()) {
 		v[k++] = v1[i++];
+		s.copy_count++;
 	}
 	while (j < v2.size()) {
 		v[k++] = v2[j++];
+		s.copy_count++;
 	}
 }
 
-void merge_sort(std::vector<int>& v) {
-	if (v.size() <= 1) return;
+stats merge_sort(std::vector<int>& v) {
+	stats s;
+	if (v.size() <= 1) return s;
 
 	size_t middle = v.size() / 2;
 	std::vector<int>v1(v.begin(), v.begin() + middle);
 	std::vector<int>v2(v.begin() + middle, v.end());
 
-	merge_sort(v1);
-	merge_sort(v2);
+	stats s1 = merge_sort(v1);
+	stats s2 = merge_sort(v2);
 
-	merge(v1, v2, v);
+	s.comparison_count = s1.comparison_count + s2.comparison_count;
+	s.copy_count = s1.copy_count + s2.copy_count;
+
+	merge(v1, v2, v, s);
+
+	return s;
 }
