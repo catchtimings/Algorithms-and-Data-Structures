26e06e2fb728e05d38274e9f90fd2c7cd9bbe86d
catchtimings <chindina.63@gmail.com>
2024-12-02 15:09:15 +0400

    init insert_sort function

---
+++ lab3/functioins.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <stdexcept>
+
+void print_vector(const std::vector<int>& v) {
+	for (size_t i = 0; i < v.size(); ++i) {
+		std::cout << v[i] << " ";
+	}
+	std::cout << '\n';
+}
+
+void insert_sort(std::vector<int>& v) {
+	if (v.size() == 0) throw std::logic_error("Vector is empty");
+
+	for (size_t i = 0; i < v.size() - 1; ++i) {
+		for (size_t j = i + 1; j < v.size(); ++j) {
+			int num1 = v[i];
+			int num2 = v[j];
+			if (num1 > num2) {
+				std::swap(v[j-1], v[j]);
+			}
+		}
+	}
+}
--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "functioins.h"
 
 int main() {
-	std::cout << "Lab3";
+	try {
+		std::vector<int> v = { 957, 26, 9365, 837 };
+		insert_sort(v);
+		print_vector(v);
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
 	return 0;
 }
