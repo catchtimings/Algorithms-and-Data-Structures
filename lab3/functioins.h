83afd2cfb06f3a9a103350a4156808c5c9baa197
catchtimings <chindina.63@gmail.com>
2024-12-02 15:12:01 +0400

    lil changes

--- lab3/functioins.h
+++ lab3/functioins.h
@@ -15,7 +15,7 @@ void insert_sort(std::vector<int>& v) {
 	for (size_t i = 0; i < v.size() - 1; ++i) {
 		for (size_t j = i + 1; j < v.size(); ++j) {
 			if (v[i] > v[j]) {
-				std::swap(v[j-1], v[j]);
+				std::swap(v[i], v[j]);
 			}
 		}
 	}
