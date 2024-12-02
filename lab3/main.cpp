1bd23c8e4ea57998c74ccf828cacb7055052bd24
catchtimings <chindina.63@gmail.com>
2024-12-02 16:43:46 +0400

    all changes

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <ctime>
-#include "functioins.h"
+#include "task1.h"
 
 int main() {
 	try {
 		srand(static_cast<int>(time(0)));
-		std::vector <int> v(5);
-		generate_vector(v);
-		//std::vector<int> v = { 6, 59, 236, 0 };
-		std::cout << "Before sort: ";
-		print_vector(v);
+		//std::vector<int> v = { 6, 59, 236, 0 }; //общий случай для проверок
+		
+		{
+			std::vector <int> v(5);
+			generate_vector(v);
+			std::cout << "Before sort: ";
+			print_vector(v);
 
-		insert_sort(v);
-		std::cout << "After sort: ";
-		print_vector(v);
+			insert_sort(v);
+			std::cout << "After sort: ";
+			print_vector(v);
+		}
+		std::cout << '\n';
+		{
+
+		}
 	}
 	catch (std::exception& e) {
 		std::cerr << e.what() << '\n';
