45d08a9bf517cf0b594265bf5048f71725be616b
catchtimings <chindina.63@gmail.com>
2024-12-03 18:13:23 +0400

    rename task1 to sorts

---
+++ lab3/sorts.h
@@ -0,0 +1,102 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include "stats.h"
+
+
+
+void fill_vector(std::vector<int>& v) {
+	for (size_t i = 0; i < v.size(); ++i) {
+		v[i] = rand() % 100 + 1;
+	}
+}
+
+void print_vector(const std::vector<int>& v) {
+	for (int num : v) {
+		std::cout << num << " ";
+	}
+	std::cout << '\n';
+}
+
+stats insert_sort(std::vector<int>& v) {
+	stats s;
+	if (v.size() == 0) return s;
+
+	
+	for (size_t i = 1; i < v.size(); ++i) {
+		int key = v[i];
+		size_t j = i;
+		while (j > 0 && v[j-1] > key) {
+			std::swap(v[j], v[j - 1]);
+			--j;
+			s.comparison_count++;
+			s.copy_count += 3;
+		}
+		v[j] = key; 
+	}
+
+	return s;
+}
+
+stats comb_sort(std::vector<int>& v) {
+	stats s;
+	if (v.size() == 0) return s;
+
+	//const double factor = 1.247;
+	size_t step = v.size() - 1;
+
+	while (step >= 1) {
+		for (size_t i = 0; i + step < v.size(); ++i) {
+			if (v[i] > v[i + step]) {
+				std::swap(v[i], v[i + step]);
+				s.comparison_count++;
+				s.copy_count += 3;
+			}
+		}
+		--step;
+	}
+	return s;
+}
+
+void merge(const std::vector<int>& v1, const std::vector<int>& v2, std::vector<int>& v, stats& s) {
+	size_t i = 0, j = 0, k = 0;
+
+	while (i < v1.size() && j < v2.size()) {
+		s.comparison_count++;
+		if (v1[i] <= v2[j]) {
+			v[k++] = v1[i++];
+		}
+		else {
+			v[k++] = v2[j++];
+		}
+		s.copy_count++;
+	}
+
+	while (i < v1.size()) {
+		v[k++] = v1[i++];
+		s.copy_count++;
+	}
+	while (j < v2.size()) {
+		v[k++] = v2[j++];
+		s.copy_count++;
+	}
+}
+
+stats merge_sort(std::vector<int>& v) {
+	stats s;
+	if (v.size() <= 1) return s;
+
+	size_t middle = v.size() / 2;
+	std::vector<int>v1(v.begin(), v.begin() + middle);
+	std::vector<int>v2(v.begin() + middle, v.end());
+
+	stats s1 = merge_sort(v1);
+	stats s2 = merge_sort(v2);
+
+	s = s1 + s2;
+
+	merge(v1, v2, v, s);
+
+	return s;
+}
